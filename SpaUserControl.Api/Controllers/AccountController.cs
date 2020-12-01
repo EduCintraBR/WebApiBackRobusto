710403b48f2407d9492c0b8baa9fc0d4f467ff81
Eduardo Cintra <ecintra@topsys.com>
2020-12-01 16:17:05 -0300

    First commit

---
+++ SpaUserControl.Api/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+﻿using SpaUserControl.Api.Models;
+using SpaUserControl.Domain.Contracts.Services;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SpaUserControl.Common.Resources;
+
+namespace SpaUserControl.Api.Controllers
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        private IUserService _service;
+
+        public AccountController(IUserService service)
+        {
+            this._service = service;
+        }
+
+        [Route("")]
+        [HttpPost]
+        public Task<HttpResponseMessage> Register(RegisterUserModel model)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                this._service.Register(model.Name, model.Email, model.Password, model.ConfirmPassword);
+                response = Request.CreateResponse(HttpStatusCode.OK, new { name = model.Name , email = model.Email });
+            }
+            catch(Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(response);
+            return tsc.Task;
+        }
+
+        [Route("")]
+        [HttpPut]
+        [Authorize]
+        public Task<HttpResponseMessage> ChangeInformation(ChangeInformationModel model)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try
+            {
+                _service.ChangeInformation(User.Identity.Name, model.Name);
+                response = Request.CreateResponse(HttpStatusCode.OK, new { name = model.Name });
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(response);
+            return tsc.Task;
+        }
+
+        [Route("change-password")]
+        [HttpPost]
+        [Authorize]
+        public Task<HttpResponseMessage> ChangePassword(ChangePasswordModel model)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try
+            {
+                _service.ChangePassword(User.Identity.Name, model.Password, model.NewPassword, model.ConfirmNewPassword);
+                response = Request.CreateResponse(HttpStatusCode.OK, Messages.PasswordSuccessfulyChanges);
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(response);
+            return tsc.Task;
+        }
+
+        [HttpPost]
+        [Route("reset-password")]
+        public Task<HttpResponseMessage> ResetPassword(ResetPasswordModel model)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try
+            {
+                var password = _service.ResetPassword(model.Email);
+                response = Request.CreateResponse(HttpStatusCode.OK, String.Format(Messages.ResetPasswordEmailBody, password));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(response);
+            return tsc.Task;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this._service.Dispose();
+        }
+    }
+}
---
+++ SpaUserControl.Api/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace SpaUserControl.Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Authorize]
+        public string Get()
+        {
+            return User.Identity.Name;
+        }
+    }
+}
